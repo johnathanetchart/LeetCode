13dcd40c4486e69d0ae4761554dbd85ef98f7f82
johnathanetchart <johnathan.etchart@gmail.com>
2021-05-19 16:53:47 -0700

    Time: 88 ms (71.36%), Space: 44.3 MB (33.72%) - LeetHub

--- single-number/single-number.js
+++ single-number/single-number.js
@@ -3,18 +3,28 @@
  * @return {number}
  */
 var singleNumber = function(nums) {
-    let sorted = nums.sort();
-    let paired;
-    for (let i = 0; i < sorted.length; i++) {
-        paired = false;
-        for (let j = 0; j < sorted.length; j++) {
-            if(i !== j && sorted[i] === sorted[j]) {
-                paired = true;
-            }
-        }
-        if(paired === false) {
-            return sorted[i];
-        }
+//     let sorted = nums.sort();
+//     let paired;
+//     for (let i = 0; i < sorted.length; i++) {
+//         paired = false;
+//         for (let j = 0; j < sorted.length; j++) {
+//             if(i !== j && sorted[i] === sorted[j]) {
+//                 paired = true;
+//             }
+//         }
+//         if(paired === false) {
+//             return sorted[i];
+//         }
         
+//     }
+    let numsObj = {};
+    for (let i = 0; i < nums.length; i++) {
+        if(numsObj[nums[i]] === undefined) {
+            numsObj[nums[i]] = nums[i];
+        } else {
+            delete numsObj[nums[i]];
+        }
     }
+    let key = Object.keys(numsObj)[0];
+    return numsObj[key];
 }
