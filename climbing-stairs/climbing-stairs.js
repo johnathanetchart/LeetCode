15cea4cb1cbe1098a7e5e4c3e95493bfbba333cc
johnathanetchart <johnathan.etchart@gmail.com>
2021-05-25 01:37:28 -0700

    Time: 80 ms (38.63%), Space: 38.2 MB (88.34%) - LeetHub

--- climbing-stairs/climbing-stairs.js
+++ climbing-stairs/climbing-stairs.js
@@ -2,19 +2,20 @@
  * @param {number} n
  * @return {number}
  */
-var climbStairs = function(n) {
-    
-    
-    if(n == 1 || n == 0 ) {
-        return 1;
-    }
-    let oneStep = 1;
-    let twoStep = 2;
-    
-    for( let i = 3; i <= n; i++) {
-        let nextStep = oneStep + twoStep;
-        oneStep = twoStep;
-        twoStep = nextStep;
-    }
-    return twoStep;
+var climbStairs = function(top) {
+    let memo = {};
+    let recursion = (current, top, memo) => {
+        if(current > top) {
+            return 0;
+        }
+        if(current === top) {
+            return 1;
+        }
+        if (memo[current]) { //try no 0
+            return memo[current];
+        }
+        memo[current] = recursion(current + 1, top, memo) + recursion(current + 2, top, memo);
+        return memo[current];
+    };
+    return recursion(0, top, memo);
 };
