075c4997fffac9390bf4f48d9c5c9287bd2173ef
johnathanetchart <johnathan.etchart@gmail.com>
2021-06-17 08:31:28 +0900

    Time: 72 ms (98.91%), Space: 40.2 MB (17.69%) - LeetHub

--- maximum-subarray/maximum-subarray.js
+++ maximum-subarray/maximum-subarray.js
@@ -4,21 +4,56 @@
  */
 
 var maxSubArray = function(nums) {
-    //
-    //
-    let bigSum = nums[0];
-    let currSum = bigSum;
-    for(let i = 0; i < nums.length; i++) {
-        currSum = nums[i];
-        if(currSum > bigSum) {
-            bigSum = currSum;
-        }
-        for(let j = i + 1; j < nums.length; j++) {
-            currSum += nums[j];
-            if(currSum > bigSum) {
-                bigSum = currSum;
-            }
-        }
+    if(nums.length === 1) {
+        return nums[0];
     }
-    return bigSum;
+    //
+    let maxSub = 0;
+    let currMax = 0;
+    let noPos = true;
+    let bigNeg = -Infinity;
+    
+    for(let i = 0; i < nums.length; i++) {
+        //need to check if there were ever any positives
+        if(nums[i] >= 0) {
+            noPos = false;
+        }
+        if(nums[i] < 0 && nums[i] > bigNeg) {
+            bigNeg = nums[i];
+        }
+        currMax += nums[i];
+        if(currMax < 0) {
+            currMax = 0;
+        }
+        if(currMax > maxSub) {
+            maxSub = currMax;
+        }
+        
+    }
+    if(noPos === true) {
+        return bigNeg;
+    }
+    return maxSub;
+    
+    
+    
+    
+    
+    
+    // O(n2)
+    // let bigSum = nums[0];
+    // let currSum = bigSum;
+    // for(let i = 0; i < nums.length; i++) {
+    //     currSum = nums[i];
+    //     if(currSum > bigSum) {
+    //         bigSum = currSum;
+    //     }
+    //     for(let j = i + 1; j < nums.length; j++) {
+    //         currSum += nums[j];
+    //         if(currSum > bigSum) {
+    //             bigSum = currSum;
+    //         }
+    //     }
+    // }
+    // return bigSum;
 };
