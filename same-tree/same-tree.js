3346534d1257aad72f06c8075b939c1e5f8da16f
johnathanetchart <johnathan.etchart@gmail.com>
2021-08-26 05:41:28 +0900

    Time: 76 ms (47.43%), Space: 39.6 MB (89.87%) - LeetHub

--- same-tree/same-tree.js
+++ same-tree/same-tree.js
@@ -12,14 +12,55 @@
  * @return {boolean}
  */
 var isSameTree = function(p, q) {
+    //check if values are the same
     if(p === null && q === null) {
         return true;
     }
     if(p === null || q === null) {
         return false;
     }
-    if(p.val != q.val) {
+    if(p.val !== q.val) {
         return false;
     }
-    return isSameTree(p.right, q.right) && isSameTree(p.left, q.left);
+    //check if they both have a left
+    if(p.left && q.left) {
+        
+    }
+    
+    //check if they both have a right
+    if(p.right && q.right) {
+        
+    }
+    //recurse
+    return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    // if(p === null && q === null) {
+    //     return true;
+    // }
+    // if(p === null || q === null) {
+    //     return false;
+    // }
+    // if(p.val != q.val) {
+    //     return false;
+    // }
+    // return isSameTree(p.right, q.right) && isSameTree(p.left, q.left);
 };
