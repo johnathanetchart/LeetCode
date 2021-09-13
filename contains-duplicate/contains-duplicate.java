1db5239058bcad39be188940d9f36057982c58a9
johnathanetchart <johnathan.etchart@gmail.com>
2021-09-14 06:25:25 +0900

    Time: 16 ms (9.85%), Space: 53.2 MB (22.82%) - LeetHub

--- contains-duplicate/contains-duplicate.java
+++ contains-duplicate/contains-duplicate.java
@@ -1,11 +1,22 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
         Arrays.sort(nums);
-        for(int i = 1; i < nums.length; i++) {
-            if(nums[i] == nums[i - 1]) {
-                return true;
-            }
+        HashSet<Integer> hash = new HashSet(20);
+        for(int i = 0; i < nums.length; i++) {
+            hash.add(nums[i]);
         }
+        // System.out.println(hash.size());
+        if(hash.size() != nums.length) {
+            return true;
+        }
+        
+        // List<Integer> listWithoutDuplicates = new ArrayList<>(new HashSet<>(nums));
+        // System.out.println(Arrays.toString(nums));
+        // for(int i = 1; i < nums.length; i++) {
+        //     if(nums[i] == nums[i - 1]) {
+        //         return true;
+        //     }
+        // }
         return false;
     }
 }
